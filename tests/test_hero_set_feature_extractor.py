5934f9ae7bcf8c86beb6834cc6d0584e47e717d3
Greg Marra <greg.marra@gmail.com>
2016-02-06 22:47:00 -0800

    Tests set features.

--- tests/test_hero_set_feature_extractor.py
+++ tests/test_hero_set_feature_extractor.py
@@ -1,10 +1,12 @@
 import unittest2
 
+from feature_extractors.hero_feature_extractor import HeroFeatureExtractor
 from feature_extractors.hero_set_feature_extractor import HeroSetFeatureExtractor
 
 class TestHeroSetFeatureExtractor(unittest2.TestCase):
     def setUp(self):
         self.hsfe = HeroSetFeatureExtractor()
+        self.hfe = HeroFeatureExtractor()
 
     def test_output(self):
         output = dict()
@@ -16,3 +18,14 @@ class TestHeroSetFeatureExtractor(unittest2.TestCase):
 
         self.assertEqual(len(self.hsfe.extractFeatureNames()), len(self.hsfe.extract(heroes)))
 
+        self.assertEqual(output["sum_ATT"], 4)
+        self.assertEqual(output["max_ATT"], 2)
+        self.assertEqual(output["min_ATT"], 0)
+        self.assertEqual(output["median_ATT"], 1)
+        self.assertEqual(output["product_ATT"], 0)
+
+        self.assertEqual(output["sum_FARM PRIORITY"], 0.6105)
+        self.assertEqual(output["max_FARM PRIORITY"], 0.162)
+        self.assertEqual(output["min_FARM PRIORITY"], 0.088)
+        self.assertEqual(output["median_FARM PRIORITY"], 0.1301)
+        self.assertEqual(output["product_FARM PRIORITY"], 2.3999519522879996e-05)
