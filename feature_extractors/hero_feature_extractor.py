775d663c61934a24ce656d2ad94cd886dd9bee4b
Greg Marra <greg.marra@gmail.com>
2016-02-06 21:04:01 -0800

    Adds farm priorities to features.

--- feature_extractors/hero_feature_extractor.py
+++ feature_extractors/hero_feature_extractor.py
@@ -6,11 +6,18 @@ class HeroFeatureExtractor(object):
 
     def __init__(self):
         super(HeroFeatureExtractor, self).__init__()
+
         self.heroes = dict()
         with open('data/heroes.csv', 'r') as f:
             heroreader = csv.reader(f)
             for hero in heroreader:
                 self.heroes[self.clean_hero_name(hero[0])] = hero[1:]
+
+        self.farm_priorities = dict()
+        with open('data/farm_priority.csv', 'rU') as f:
+            heroreader = csv.reader(f)
+            for hero in heroreader:
+                self.farm_priorities[self.clean_hero_name(hero[0])] = hero[1:]
 
     def clean_hero_name(self, hero_name_str):
         return re.sub('[^A-Za-z0-9]+', '', hero_name_str)
@@ -19,11 +26,15 @@ class HeroFeatureExtractor(object):
         """Returns attributes related to a particular hero."""
         features = list()
 
-        features.extend(self.getHeroStats(self.clean_hero_name(hero_name_str)))
+        clean_hero_name = self.clean_hero_name(hero_name_str)
+        features.extend(self.getHeroStats(clean_hero_name))
+        features.extend(self.getHeroFarmPriority(clean_hero_name))
 
         return features
 
 
     def getHeroStats(self, hero_name_str):
-        """Returns the index of the hero in the hero list."""
         return [float(n) for n in self.heroes[hero_name_str][1:]]
+
+    def getHeroFarmPriority(self, hero_name_str):
+        return [float(n) for n in self.farm_priorities[hero_name_str][1:]]
